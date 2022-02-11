93f18576b5c006d81ee7dab5fd41a5112ca43699
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-11 21:59:38 +0800

    Added CSS JS Fundamentals 2

---
+++ fundamentals2/script.js
@@ -0,0 +1,6 @@
+let add7 = num => num + 7;
+let multiply = (mul1, mul2) => mul1 * mul2;
+let capitalize = strCapital => strCapital.charAt(0).toUpperCase() + str.slice(1);
+let lastLetter = strLast => strLast.slice(-1);
+
+console.log(lastLetter("KarlEspiritu"));
