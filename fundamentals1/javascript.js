fd22c70f5b6d869a3169c7c04f9f81c253159da1
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-11 17:02:33 +0800

    Added fundamentals1 from JS section

---
+++ fundamentals1/javascript.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+/*Working with Variables*/
+// let admin;
+// let user = "John";
+// console.log(admin = user);
+// alert(admin);
+
+/*Giving the right name*/
+// let ourPlanetName = "Earth"
+// let currentVisitor = "John"
+
+/*Assignment*/
+// console.log(23 + 97);
+// console.log(1 + 2 + 3 + 4 + 5 + 6);
+// console.log((4 + 6 + 9) / 77);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
