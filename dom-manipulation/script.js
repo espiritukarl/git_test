9af249e38d085a7ea85136726f458a1950f71c57
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-13 17:47:34 +0800

    Added Event Listener practice files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,4 @@
+//DOM MANIPULATION
 const content = document.querySelector(".content");
 
 // create p in div.content
@@ -29,3 +30,24 @@ divBorder.appendChild(header);
 divBorder.appendChild(parInH1);
 
 content.appendChild(divBorder);
+
+//EVENTS LISTEN
+const btn2 = document.querySelector("#btn2");
+btn2.onclick = () => alert("Hello World!");
+
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener('click', () => {
+    alert("Hello World!");
+});
+
+const btn = document.querySelector("#btn")
+btn.addEventListener('click', (e) => {
+    e.target.style.background = 'blue';
+});
+
+const buttons = document.querySelectorAll("#container>button");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
